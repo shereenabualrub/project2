726fdab96b6d0955b9d6e43de6830731befddb69
Shereen Abu Alrub <150395841+shereenabualrub@users.noreply.github.com>
2024-08-31 03:17:57 +0300

    Update and rename almosafer2.cy.js to project2.cy.js

--- cypress/e2e/project2.cy.js
+++ cypress/e2e/project2.cy.js
@@ -13,14 +13,15 @@ describe('test', () => {
 
     cy.visit('https://www.almosafer.com/ar?ncr=1')
 
-   let websites = ["https://global.almosafer.com/en?gad_source=1&gclid=CjwKCAiAyp-sBhBSEiwAWWzTnop4k7SH0sDrITOYbo9LRYigvgK5gN_rAmjFMxfu1gX2lEuGMXzIKBoCdY8QAvD_BwE", "https://www.almosafer.com/ar?ncr=1"]
+    let websites = ["https://global.almosafer.com/en?gad_source=1&gclid=CjwKCAiAyp-sBhBSEiwAWWzTnop4k7SH0sDrITOYbo9LRYigvgK5gN_rAmjFMxfu1gX2lEuGMXzIKBoCdY8QAvD_BwE", "https://www.almosafer.com/ar?ncr=1"]
     let RandomIndex = Math.floor(Math.random()*websites.length)
     cy.visit(websites[RandomIndex])
     cy.get('#uncontrolled-tab-example-tab-hotels').click()
+    
     if (RandomIndex==0) {
       cy.get('[data-testid="AutoCompleteInput"]').type("dubai")
       
-    } else if(RandomIndex== 1) {
+     } else if(RandomIndex== 1) {
       
       cy.get('[data-testid="AutoCompleteInput"]').type("دبي")
     }
